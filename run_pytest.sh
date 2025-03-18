89346fbcfb60c6fc8763677370bd8fced3aaff21
Helen Ren <helen01@Helens-MacBook-Pro.local>
2025-03-18 19:53:53 -0400

    move pytest stuff to a script

---
+++ run_pytest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "BEFORE: ls -l /usr/lib/x86..."
+ls -l /usr/lib/x86_64-linux-gnu
+sudo apt-get update
+sudo apt-get install libmtdev1t64
+echo "AFTER: ls -l /usr/lib/x86..."
+ls -l /usr/lib/x86_64-linux-gnu
+export KIVY_HOME=/Users/runner/work/tests_mymodule/tests_mymodule
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+pipenv install kivy
+pipenv install pytest
+pipenv run python -m pytest
+cat ./pytest-log.txt
