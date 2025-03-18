51c00ce77823071745b038d487ea460cbd6dd332
Helen Ren <helen01@Helens-MacBook-Pro.local>
2025-03-18 12:49:44 -0400

    change build file to windows-latest

--- src/mymodule/mylib.py
+++ src/mymodule/mylib.py
@@ -1,7 +1,6 @@
-# from kivy.app import App
-# from kivy.uix.label import Label
-# from kivy.uix.boxlayout import BoxLayout
-# from kivy.core.window import Window
+from kivy import Config
+Config.set('kivy', 'log_level', 'debug')
+
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
