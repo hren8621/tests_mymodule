d63d1962fa16e773f34d5f4ed21566e1b66a826f
Helen Ren <helen01@Helens-MacBook-Pro.local>
2025-03-18 14:52:12 -0400

    add KIVY_USE_DEFAULTCONFIG at the beginning of test_mylib.py

--- tests/test_mylib.py
+++ tests/test_mylib.py
@@ -1,7 +1,7 @@
 import os
 os.environ['KIVY_HOME'] = "/Users/runner/work/tests_mymodule/tests_mymodule"
 os.environ['KIVY_USE_DEFAULTCONFIG']='1'
-
+import kivy
 import pytest
 # from pytest_mock import MockerFixture
 from mymodule import mylib
