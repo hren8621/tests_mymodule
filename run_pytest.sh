221ebcaf4f9e92cdba8c8874a9d2b4cdc7e772a9
Helen Ren <helen01@Helens-MacBook-Pro.local>
2025-03-18 19:58:46 -0400

    fix typo in run_pytest.sh and make script executable

--- run_pytest.sh
+++ run_pytest.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install libmtdev1t64
 echo "AFTER: ls -l /usr/lib/x86..."
 ls -l /usr/lib/x86_64-linux-gnu
-export KIVY_HOME=/Users/runner/work/tests_mymodule/tests_mymodule
+export KIVY_HOME=/home/runner/work/tests_mymodule/tests_mymodule
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 pipenv install kivy
 pipenv install pytest
