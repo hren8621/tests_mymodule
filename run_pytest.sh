74422e351d9bbf638aff890d39786524f315d1e4
Helen Ren <helen01@Helens-MacBook-Pro.local>
2025-03-18 20:11:01 -0400

    cat output files from pytest

--- run_pytest.sh
+++ run_pytest.sh
@@ -9,5 +9,5 @@ export KIVY_HOME=/home/runner/work/tests_mymodule/tests_mymodule
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 pipenv install kivy
 pipenv install pytest
-pipenv run python -m pytest
+pipenv run python -m pytest >> ./run-pytest-log.txt
 cat ./pytest-log.txt
